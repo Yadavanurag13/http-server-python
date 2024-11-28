40d3e78627994efb3d1cf4a795805a1861f18b75
Anurag Yadav <yadavanurag1310@gmail.com>
2024-11-28 21:13:42 +0530

    implemented socket send

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
 import socket  # noqa: F401
 
 
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    sock, add = server_socket.accept()
+    sock.send(b'HTTP/1.1 200 OK\r\n\r\n')
+    sock.close()
+    
 
 
 if __name__ == "__main__":
