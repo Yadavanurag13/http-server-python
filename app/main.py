245aa7922a29e3f26a20e7cffd3fb4f724e6638b
Anurag Yadav <yadavanurag1310@gmail.com>
2024-12-01 00:17:45 +0530

    added concurrent support

--- app/main.py
+++ app/main.py
@@ -1,18 +1,10 @@
-import socket  # noqa: F401
+import socket 
+import threading
 
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    sock, add = server_socket.accept()
-
+def handle_connection(sock, addr):
     req = sock.recv(1024).decode()
 
     path = req.split("\r\n")[0].split(" ")[1]
-
-    print(req)
-
 
     if path == "/":
         sock.send(b'HTTP/1.1 200 OK\r\n\r\n')
@@ -23,7 +15,6 @@ def main():
     elif path.startswith("/user-agent"):
         content = req.split(" ")[-1].split()[-1]
         response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(content)}\r\n\r\n{content}"
-
         sock.send(response.encode())
     else:
         sock.send(b'HTTP/1.1 404 Not Found\r\n\r\n')
@@ -31,6 +22,18 @@ def main():
     sock.close()
 
 
+
+def main():
+    # You can use print statements as follows for debugging, they'll be visible when running tests.
+    print("Logs from your program will appear here!")
+
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+
+    while True:
+        sock, add = server_socket.accept()
+        thread = threading.Thread(target=handle_connection, args=(sock, add))
+        thread.start()
+        
 
 if __name__ == "__main__":
     main()
