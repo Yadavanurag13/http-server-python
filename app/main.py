26b6ad53420c9fd07abe40b4d3890157afab27b6
Anurag Yadav <yadavanurag1310@gmail.com>
2024-12-01 18:25:01 +0530

    post method done

--- app/main.py
+++ app/main.py
@@ -3,9 +3,13 @@ import threading
 import sys
 
 def handle_connection(sock, addr):
+    
     req = sock.recv(1024).decode()
 
     path = req.split("\r\n")[0].split(" ")[1]
+
+    print(req)
+    print(path)
 
     if path == "/":
         sock.send(b'HTTP/1.1 200 OK\r\n\r\n')
@@ -18,24 +22,43 @@ def handle_connection(sock, addr):
         response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(content)}\r\n\r\n{content}"
         sock.send(response.encode())
     elif path.startswith("/files"):
-        directory = sys.argv[2]
-        filename = path[7:]
 
-        print(directory)
-        print(filename)
-        try:
-            with open(f"/{directory}/{filename}", "r") as f:
-                body = f.read()
+        method = req.split("\r\n")[0].split(" ")[0]
 
-                print(body)
-                response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(body)}\r\n\r\n{body}"
+        if method == "GET":
+            directory = sys.argv[2]
+            filename = path[7:]
+
+            try:
+                with open(f"/{directory}/{filename}", "r") as f:
+                    body = f.read()
+
+                    # print(body)
+                    response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(body)}\r\n\r\n{body}"
+                    sock.send(response.encode())
+            except:
+                response = f"HTTP/1.1 404 Not Found\r\n\r\n"
                 sock.send(response.encode())
-        except:
-            response = f"HTTP/1.1 404 Not Found\r\n\r\n"
-            sock.send(response.encode())
+        
+        if method == "POST":
+            directory = sys.argv[2]
+            filename = path[7:]
+
+            body = req.split("\r\n\r\n")[1]
+
+            print(method, directory, filename, body)
+            try:
+                file_path = f"{directory}/{filename}"
+                with open(file_path, "w") as filename:
+                    filename.write(body)
+
+                response = f"HTTP/1.1 201 Created\r\n\r\n"
+                sock.send(response.encode())
+            except:
+                pass
     else:
         sock.send(b'HTTP/1.1 404 Not Found\r\n\r\n')
-        
+
     sock.close()
 
 
